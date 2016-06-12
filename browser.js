2c5d0f37d6d15bfe5baed23c391803080f8b32da
Atsushi Takayama <taka.atsushi@gmail.com>
2016-06-12 14:02:39 +0900

    Client side React rendering

--- browser.js
+++ browser.js
@@ -1,1 +1,8 @@
-console.log(0);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+
+ReactDOM.render(
+  <Main />
+  , document.getElementById('app')
+);
