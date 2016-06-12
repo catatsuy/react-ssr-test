501620ad931478c299cf7d1eb813902812d191e8
Atsushi Takayama <taka.atsushi@gmail.com>
2016-06-12 17:38:58 +0900

    Async-props on client side

--- browser.js
+++ browser.js
@@ -4,11 +4,12 @@ import { Router, browserHistory } from 'react-router'
 import Main from './components/Main';
 import Leaf from './components/Leaf';
 import routes from './routes';
+import AsyncProps from 'async-props'
 
 const appElem = document.getElementById('app');
 const json = appElem.dataset.initialProps;
 const initialProps = JSON.parse(json);
 
 render((
-  <Router history={browserHistory} routes={routes} />
+  <Router history={browserHistory} routes={routes} render={(props) => <AsyncProps {...props}/>} />
 ), appElem)
--- components/Main.jsx
+++ components/Main.jsx
@@ -2,26 +2,13 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      info: props.time ? props.time : 'loading ... ',
-    };
-  }
-
-  componentDidMount() {
-    if (this.state.info === 'loading ... ') {
-      this.getInfo();
-    }
-  }
-
-  getInfo() {
+  static loadProps(params, cb) {
     fetch('http://localhost:9901/time', {
-      headers: { 'x-csrf-token': this.props.csrfToken },
+      // headers: { 'x-csrf-token': this.props.csrfToken },
     })
     .then((result) => result.json())
     .then((res) => {
-      this.setState({ info: res.time });
+      cb(null, { info: res.time });
     });
   }
 
@@ -30,7 +17,7 @@ class Main extends React.Component {
       <div className="main">
         <header>
           <h1><a href="/">React SSR Sample</a></h1>
-          <p>{this.state.info}</p>
+          <p>{this.props.info}</p>
           <Link to="/leaf">leaf</Link>
         </header>
       </div>
@@ -38,9 +25,9 @@ class Main extends React.Component {
   }
 }
 
-Main.propTypes = {
-  time: React.PropTypes.number,
-  csrfToken: React.PropTypes.string.isRequired,
-};
+// Main.propTypes = {
+//   time: React.PropTypes.number,
+//   csrfToken: React.PropTypes.string.isRequired,
+// };
 
 export default Main;
